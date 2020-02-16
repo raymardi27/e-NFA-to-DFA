ec081b3ff591982ed03b4a9703419fc12e1a15d5
raymardi27 <raygit19@gmail.com>
2020-02-16 13:33:42 +0530

    Slight Change in Input method

--- NFA-DFA.py
+++ NFA-DFA.py
@@ -42,7 +42,6 @@ def trans_table(transitions,n=0):#To print the transition table
 def get_closure(transitions):#To calculate epsilon closure values
     sigma = get_sigma(transitions)
     Transitions = complete(transitions,sigma)
-    # for _ in range(2):
     for i in Transitions:
         eps_list = [i]
         if Transitions[i]['eps'] != '-':
@@ -113,17 +112,17 @@ def print_DFA_tt(transitions,aux):
     print(tabulate(Pri_list,headers=header_list))
 
 if __name__ == "__main__":
-    # file = open("data2.txt",'r+')
+    file = open("data2.txt",'r+')
     main_dic = dict() #main is the one having the state and its transitions.
-    num = int(input("Input the number of states: "))
-    # num = int(file.readline())
-    print("Enter the Transition states: (Format- <input>:<list of states>;<input>:<list of states>. Epsilon should be mentioned as 'eps' only! Do not put the epsilon transition for the same state!)")
+    # num = int(input("Input the number of states: "))
+    num = int(file.readline())
+    # print("Enter the Transition states: (Format- <input>:<list of states>;<input>:<list of states>. Epsilon should be mentioned as 'eps' only! Do not put the epsilon transition for the same state!)")
     for i in range(num):
         state = 'q'+str(i)
-        string = input("For "+state+" : ").split(";")
-        # string = file.readline().split(";")
-        # for i in range(len(string)):
-        #     string[i] = string[i].rstrip('\n')
+        # string = input("For "+state+" : ").split(";")
+        string = file.readline().split(";")
+        for i in range(len(string)):
+            string[i] = string[i].rstrip('\n')
         inner_dict = dict() #inner is the one with input and the next states.
         for part in string:
             ip = sub(r':.*','',part) #The first part of the semi-colon is the inputs
@@ -131,10 +130,10 @@ if __name__ == "__main__":
             if tr[0] != '':
                 inner_dict[ip] = tr
         main_dic[state] = inner_dict
-    # imp = file.readline().rstrip('\n').split()
-    # fmp = file.readline().rstrip('\n').split()
-    imp = input("Input the initial states: ").split()
-    fmp = input("Input the final states: ").split()
+    imp = file.readline().rstrip('\n').split()
+    fmp = file.readline().rstrip('\n').split()
+    # imp = input("Input the initial states: ").split()
+    # fmp = input("Input the final states: ").split()
     for i in main_dic:
         if i in imp:
             main_dic[i][' Stat'] = 'Ini'
