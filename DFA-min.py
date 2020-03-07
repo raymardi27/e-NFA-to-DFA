aeac4ad4c030243115af2f81ca7bfc731f28c413
raymardi27 <raygit19@gmail.com>
2020-03-07 21:01:10 +0530

    Learnt Deep-copy, and using something to make it work

--- DFA-min.py
+++ DFA-min.py
@@ -1,6 +1,36 @@
 from re import sub
+from copy import deepcopy
 from tabulate import tabulate
 from NFA_DFA import get_sigma,complete,trans_table
+
+def divide(i,P,transitions):
+    sigma = get_sigma(transitions)
+    sigma.remove(' Stat')
+    Dup = deepcopy(transitions)
+    for k in sigma:
+        for  j in i:
+            if transitions[j][k][0] in i:
+                Dup[j][str(k)+" tr"] = 'Same'
+            else:
+                for l in P:
+                    if l != i:
+                        if transitions[j][k][0] in l:
+                            Dup[j][str(k)+" tr"] = str(l)
+    trans_table(Dup)
+
+
+def minimize(transitions):
+    P,a,b = list(),list(),list()
+    for i in transitions:
+        if transitions[i][' Stat'] == 'Fin' or transitions[i][' Stat'] == 'Ini & Fin':
+            b.append(i)
+        else:
+            a.append(i)
+    P.append(a);P.append(b)
+    for i in P:
+        divide(i,P,transitions)
+
+
 
 if __name__ == "__main__":
     file = open("dfadata.txt",'r+')
@@ -35,5 +65,6 @@ if __name__ == "__main__":
         else:
             main_dic[i][' Stat'] = ''
     trans_table(main_dic)
+    minimize(main_dic)
     
     
